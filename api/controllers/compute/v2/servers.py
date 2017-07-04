8ecfb99a517efc3ef1aaabec051d7cf4dc1ebedb
Simon Murray <spjmurray@yahoo.co.uk>
2017-07-04 15:22:26 +0000

    Flesh Out Compute Servers
    Bit of challenge this one as the restful interface uses actions upon a collection
    or a specific resource.  Luckily it seems this pattern is supported natively
    via the _custom_actions dict.  Also improved the scoping rules to support the
    all_tenants flag correctly rather than just implying it.

--- api/controllers/compute/v2/servers.py
+++ api/controllers/compute/v2/servers.py
@@ -14,25 +14,83 @@
 
 """Root controller for /compute/v2/%{tenant_id}/servers"""
 
+import oslo_utils.strutils
 import pecan
+import pecan.decorators
+import pecan.rest
 
 from sentinel.clients import Clients
+from sentinel.whitelist import Whitelist
 
-class ComputeV2ServersController(object):
+
+def _all_projects():
+    """Do we want all projects in out domain"""
+
+    # A basic copy of nova API
+    all_projects = pecan.request.GET.get('all_tenants')
+    if all_projects:
+        return oslo_utils.strutils.bool_from_string(all_projects, True)
+
+    # An empty string is considered consent
+    return 'all_tenants' in pecan.request.GET
+
+
+def _scoped_projects():
+    """Return a list of projects within our domain scope"""
+
+    domain = pecan.request.context['domain']
+    keystone = Clients.keystone(pecan.request.context['conf'])
+    projects = keystone.projects.list()
+
+    # Assumes that domains cannot be nested
+    projects = filter(lambda x: x.domain_id == domain, projects)
+    return map(lambda x: x.id, projects)
+
+
+def _scoped_servers():
+    """Return a detailed list of servers based on scoping requirements"""
+
+    # If the client requested all projects return those within the
+    # domain scope, else just that indicated by the token binding
+    if _all_projects():
+        projects = _scoped_projects()
+    else:
+        projects = [pecan.request.context['token'].project_id]
+
+    # Must do a detailed search here as it returns the tenant_id field
+    nova = Clients.nova(pecan.request.context['conf'])
+    servers = nova.servers.list(search_opts={'all_tenants': 'True'})
+
+    # Filter out only servers within the IdP domain scope
+    return filter(lambda x: x.tenant_id in projects, servers)
+
+
+class ComputeV2ServersController(pecan.rest.RestController):
+    """Basic REST controller for server access"""
+
+    def __init__(self):
+        self._custom_actions = {
+            'detail': ['GET'],
+        }
+
+    @pecan.expose('json')
+    @pecan.decorators.accept_noncanonical
+    def get_all(self):
+        """Return a list of servers scoped to the domain/project"""
+
+        servers = _scoped_servers()
+        payload = {
+            u'servers': map(lambda x: { u'id': x.id, u'name': x.name }, servers),
+        }
+
+        return payload
+
     @pecan.expose('json')
     def detail(self):
-        domain = pecan.request.context['domain']
-        keystone = Clients.keystone(pecan.request.context['conf'])
-        projects = keystone.projects.list()
-        projects = filter(lambda x: x.domain_id == domain, projects)
-        projects = map(lambda x: x.id, projects)
-
-        nova = Clients.nova(pecan.request.context['conf'])
-        servers = nova.servers.list(search_opts={'all_tenants': 'True'})
-        servers = filter(lambda x: x.tenant_id in projects, servers)
+        """Return a detailed list of servers scoped to the domain/project"""
 
         payload = {
-            'servers': map(lambda x: x.to_dict(), servers),
+            u'servers': Whitelist.apply(_scoped_servers()),
         }
 
         return payload
